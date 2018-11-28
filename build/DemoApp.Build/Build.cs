99d6307f4bb484b830b80459e2e2b615bf912ff2
Joey <brownjn12@gmail.com>
2018-11-27 22:55:15 -0600

    add full build

--- build/DemoApp.Build/Build.cs
+++ build/DemoApp.Build/Build.cs
@@ -5,9 +5,12 @@ using Nuke.Common.ProjectModel;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
+    AbsolutePath SourceDirectory => RootDirectory / "src/DemoApp.Web";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
@@ -16,18 +19,22 @@ class Build : NukeBuild
     Target Clean => _ => _
         .Executes(() =>
         {
+            DeleteDirectories(GlobDirectories(SourceDirectory, "bin", "obj"));
+            EnsureCleanDirectory(OutputDirectory);
         });
 
     Target Restore => _ => _
         .DependsOn(Clean)
         .Executes(() =>
         {
+            DotNetRestore(SourceDirectory);
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(SourceDirectory);
         });
     
 }
