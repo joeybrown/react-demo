a85238917460d88b1dded282604ed00d64b60469
Joey <brownjn12@gmail.com>
2018-11-30 23:04:37 -0600

    added npm build

--- build/DemoApp.Build/Build.cs
+++ build/DemoApp.Build/Build.cs
@@ -2,46 +2,80 @@ using System;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Npm;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.Npm.NpmTasks;
 
-class Build : NukeBuild
-{
+class Build : NukeBuild {
     AbsolutePath SourceDirectory => RootDirectory / "src/DemoApp.Web";
     AbsolutePath OutputDirectory => RootDirectory / "output";
-    public static int Main () => Execute<Build>(x => x.Copy_Assets);
+    AbsolutePath ClientAppDirectory => SourceDirectory / "ClientApp";
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    public static int Main () => Execute<Build> (x => x.Copy_Assets);
+
+    [Parameter ("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly string Configuration = IsLocalBuild ? "Debug" : "Release";
 
     Target Clean => _ => _
-        .Executes(() =>
-        {
-            DeleteDirectories(GlobDirectories(SourceDirectory, "bin", "obj"));
-            EnsureCleanDirectory(OutputDirectory);
+        .Executes (() => {
+            DeleteDirectories (GlobDirectories (SourceDirectory, "bin", "obj"));
+            EnsureCleanDirectory (OutputDirectory);
         });
 
     Target Restore => _ => _
-        .DependsOn(Clean)
-        .Executes(() =>
-        {
-            DotNetRestore(SourceDirectory);
+        .DependsOn (Clean)
+        .Executes (() => {
+            Npm ("install", ClientAppDirectory);
+            DotNetRestore (SourceDirectory);
         });
 
     Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-            DotNetBuild(SourceDirectory);
+        .DependsOn (Restore)
+        .Executes (() => {
+            DotNetBuild (SourceDirectory);
+            npmBuild ();
         });
+
+    private void npmBuild () {
+        //todo: adjust for Release
+        Npm ("run build", ClientAppDirectory);
+    }
 
     Target Copy_Assets => _ => _
-        .DependsOn(Compile)
-        .Executes(() => {
-            var binDir = SourceDirectory / "bin/Debug/netcoreapp2.1";
-            CopyDirectoryRecursively(binDir, OutputDirectory);
+        .DependsOn (Compile)
+        .Executes (() => {
+            var binDir = getBinDir ();
+            var binOutputDir = getBinOutputDir ();
+            CopyDirectoryRecursively (binDir, binOutputDir);
+
+            var clientAppDir = getClientAppDir ();
+            var clientAppOutputDir = getGlientAppOutputDir ();
+            CopyDirectoryRecursively (clientAppDir, clientAppOutputDir);
         });
 
+    private AbsolutePath getBinDir () {
+        //todo: adjust for Release
+        var binDir = SourceDirectory / "bin/Debug/netcoreapp2.1";
+        return binDir;
+    }
+
+    private AbsolutePath getBinOutputDir () {
+        var binOutputDir = OutputDirectory / "bin";
+        return binOutputDir;
+    }
+
+    private AbsolutePath getClientAppDir () {
+        //todo: adjust for Release
+        var clientAppDir = ClientAppDirectory / "build";
+        return clientAppDir;
+    }
+
+    private AbsolutePath getGlientAppOutputDir () {
+        var clientAppOutputDir = OutputDirectory / "ClientApp/build";
+        return clientAppOutputDir;
+    }
 }
